c8ccc8709034f21c76ef9476ee21349080bae48c
kham1508 <kyle.hamilton@nisra.gov.uk>
2022-06-23 12:24:28 +0100

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,27 @@
+import static_adapter from '@sveltejs/adapter-static';
+
+const dev = 'production' === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: static_adapter({
+			// Options below are defaults
+			pages: 'docs',
+			assets: 'docs',
+			//fallback:'404.html'
+		}),
+		prerender: {
+			enabled: false
+		},
+		ssr: false,
+		paths: {
+			//assets: dev ? 'https://bothness.github.io/area-explorer' : '',
+			base: dev ? '/host_test' : ''
+		}
+	}
+};
+
+export default config;
